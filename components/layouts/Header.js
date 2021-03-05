f8bdaf7ccfe4b3422a529d1f2812479545f41cd2
DiegoMutre <tysonmutre10@gmail.com>
2021-03-05 07:03:38 -0500

    styles: Create styles components Heading, Logo and HeaderContainer

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,13 +1,40 @@
 import Link from "next/link";
+import styled from "styled-components";
 import Searcher from "../ui/Searcher";
 import Nav from "./Nav";
 
+const Heading = styled.header`
+    border-bottom: 2px solid var(--gray3);
+    padding: 1rem 0;
+`;
+
+const HeaderContainer = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--orange);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: "Roboto Slab", serif;
+    margin-right: 2rem;
+`;
+
 const Header = () => {
     return (
-        <header>
-            <div>
+        <Heading>
+            <HeaderContainer>
                 <div>
-                    <p>P</p>
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
 
                     <Searcher />
 
@@ -19,8 +46,8 @@ const Header = () => {
                     <Link href="/">Login</Link>
                     <Link href="/">Create Account</Link>
                 </div>
-            </div>
-        </header>
+            </HeaderContainer>
+        </Heading>
     );
 };
 
