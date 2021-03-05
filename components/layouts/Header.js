b222a557b64495b4818abc0724ee21275689aaf2
DiegoMutre <tysonmutre10@gmail.com>
2021-03-05 05:12:23 -0500

    feat: Create Header.js

---
+++ components/layouts/Header.js
@@ -0,0 +1,20 @@
+import Searcher from "../ui/Searcher";
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <div>
+                    <p>P</p>
+
+                    <Searcher />
+
+                    {/* Nav here */}
+                </div>
+                <div>{/* Administration Menu here */}</div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
