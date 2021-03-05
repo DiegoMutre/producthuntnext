bb708a8870eea503b8e0fc23ae4a6bda7205c21b
DiegoMutre <tysonmutre10@gmail.com>
2021-03-05 05:22:27 -0500

    feat: Create Administration Menu in Header.js

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Searcher from "../ui/Searcher";
 import Nav from "./Nav";
 
@@ -12,7 +13,12 @@ const Header = () => {
 
                     <Nav />
                 </div>
-                <div>{/* Administration Menu here */}</div>
+                <div>
+                    <p>Hey there, Diego!</p>
+                    <button>Log out</button>
+                    <Link href="/">Login</Link>
+                    <Link href="/">Create Account</Link>
+                </div>
             </div>
         </header>
     );
