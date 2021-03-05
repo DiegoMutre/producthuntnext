5a3a346bfe39184a4844fc3b2601c0b97cdc1e29
DiegoMutre <tysonmutre10@gmail.com>
2021-03-05 05:12:12 -0500

    feat: Create Searcher.js

---
+++ components/ui/Searcher.js
@@ -0,0 +1,11 @@
+const Searcher = () => {
+    return (
+        <form>
+            <input type="text" />
+
+            <button type="submit">Search</button>
+        </form>
+    );
+};
+
+export default Searcher;
