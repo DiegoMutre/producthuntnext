0f34e5374f6b1e4956da24f5a44640c646280229
DiegoMutre <tysonmutre10@gmail.com>
2021-03-06 15:48:32 -0500

    feat: Create search page

---
+++ pages/search.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layouts/layout";
+
+const Search = () => {
+    return (
+        <>
+            <Layout>
+                <h1>Search</h1>
+            </Layout>
+        </>
+    );
+};
+
+export default Search;
