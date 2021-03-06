6489f0ce858fe052d662b8806d01e3e8854a48ec
DiegoMutre <tysonmutre10@gmail.com>
2021-03-06 15:26:56 -0500

    styles: Finish Searcher.js styles

--- components/ui/Searcher.js
+++ components/ui/Searcher.js
@@ -1,10 +1,40 @@
+import styled from "styled-components";
+
+const Form = styled.form`
+    position: relative;
+`;
+
+const InputText = styled.input`
+    border: 1px solid var(--gray3);
+    padding: 1rem;
+    min-width: 300px;
+`;
+
+const ButtonSubmit = styled.button`
+    height: 3rem;
+    width: 3rem;
+    display: block;
+    background-size: 4rem;
+    background-image: url("/static/img/search.png");
+    background-repeat: no-repeat;
+    position: absolute;
+    right: 1rem;
+    top: 1px;
+    background-color: #fff;
+    border: none;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
 const Searcher = () => {
     return (
-        <form>
-            <input type="text" />
+        <Form>
+            <InputText type="text" placeholder="Search Products" />
 
-            <button type="submit">Search</button>
-        </form>
+            <ButtonSubmit type="submit"></ButtonSubmit>
+        </Form>
     );
 };
 
