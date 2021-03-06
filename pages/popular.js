32e4123d584d76ecaec7edd2952563cfe0548417
DiegoMutre <tysonmutre10@gmail.com>
2021-03-06 15:42:33 -0500

    feat: Create popular page

---
+++ pages/popular.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layouts/layout";
+
+const Popular = () => {
+    return (
+        <>
+            <Layout>
+                <h1>Popular</h1>
+            </Layout>
+        </>
+    );
+};
+
+export default Popular;
