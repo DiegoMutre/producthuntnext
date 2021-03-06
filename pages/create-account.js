41b5079207b25998888d8c9e572e20d5fb049292
DiegoMutre <tysonmutre10@gmail.com>
2021-03-06 15:40:34 -0500

    feat: Create create-account page

---
+++ pages/create-account.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layouts/layout";
+
+const CreateAccount = () => {
+    return (
+        <>
+            <Layout>
+                <h1>Create Account</h1>
+            </Layout>
+        </>
+    );
+};
+
+export default CreateAccount;
