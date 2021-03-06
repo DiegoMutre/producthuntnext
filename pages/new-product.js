d82606d47947b09bab676a43a5432b384596bf44
DiegoMutre <tysonmutre10@gmail.com>
2021-03-06 15:43:38 -0500

    feat: Create new-product page

---
+++ pages/new-product.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layouts/layout";
+
+const NewProduct = () => {
+    return (
+        <>
+            <Layout>
+                <h1>New Product</h1>
+            </Layout>
+        </>
+    );
+};
+
+export default NewProduct;
