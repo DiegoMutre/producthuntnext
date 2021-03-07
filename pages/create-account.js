f7f61dcbe26c10cde6926628ebbdc736ee5d3be9
DiegoMutre <tysonmutre10@gmail.com>
2021-03-06 20:11:09 -0500

    feat: Create create-account jsx structure

--- pages/create-account.js
+++ pages/create-account.js
@@ -5,6 +5,36 @@ const CreateAccount = () => {
         <>
             <Layout>
                 <h1>Create Account</h1>
+                <form>
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            id="username"
+                            placeholder="Your name"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Your email"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="Your password"
+                        />
+                    </div>
+                    <input type="submit" value="Create Account" />
+                </form>
             </Layout>
         </>
     );
