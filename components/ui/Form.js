20f0b6e8a9873cb2cecef9edee9bee709f9914eb
DiegoMutre <tysonmutre10@gmail.com>
2021-03-06 20:26:24 -0500

    styles: Create Field styled component

--- components/ui/Form.js
+++ components/ui/Form.js
@@ -5,3 +5,19 @@ export const Form = styled.form`
     width: 95%;
     margin: 5rem auto 0 auto;
 `;
+
+export const Field = styled.div`
+    margin-bottom: 2rem;
+    display: flex;
+    align-items: center;
+
+    label {
+        flex: 0 0 150px;
+        font-size: 1.8rem;
+    }
+
+    input {
+        flex: 1;
+        padding: 1rem;
+    }
+`;
