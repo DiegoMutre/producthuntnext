32fa7be99170527e786ff8e05cc8911a11da1104
DiegoMutre <tysonmutre10@gmail.com>
2021-03-06 20:29:06 -0500

    styles: Create InputSubmit styled component

--- components/ui/Form.js
+++ components/ui/Form.js
@@ -21,3 +21,20 @@ export const Field = styled.div`
         padding: 1rem;
     }
 `;
+
+export const InputSubmit = styled.input`
+    background-color: var(--orange);
+    width: 100%;
+    padding: 1.5rem;
+    text-align: center;
+    color: #fff;
+    font-size: 1.8rem;
+    text-transform: uppercase;
+    border: none;
+    font-family: "PT Sans", sans-serif;
+    font-weight: 700;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
