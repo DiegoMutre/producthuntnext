7452f45e8747cf493d9c0e42cccf9c28cc6f7009
DiegoMutre <tysonmutre10@gmail.com>
2021-03-06 22:44:58 -0500

    feat: Create validateCreateAccount.js

---
+++ validation/validateCreateAccount.js
@@ -0,0 +1,26 @@
+export default function validateCreateAccount(values) {
+    let errors = {};
+    const regularExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+    if (!values.username) {
+        errors.username = "The username is required";
+    }
+
+    if (!values.email) {
+        errors.email = "The email is required";
+    }
+
+    if (!regularExp.test(values.email)) {
+        errors.email = "Invalid email";
+    }
+
+    if (!values.password) {
+        errors.password = "The password is required";
+    }
+
+    if (values.password.length < 6) {
+        errors.password = "The password must be at least 6 characters long";
+    }
+
+    return errors;
+}
