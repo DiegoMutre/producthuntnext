24d35224f164afa06c1cbab4d03f462dc74b302e
DiegoMutre <tysonmutre10@gmail.com>
2021-03-08 04:43:26 -0500

    feat: Add firebae config

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYs-3H_dhDZdp8wn5fqwpYsu0Bvm_TPyQ",
+    authDomain: "product-hunt-d3f12.firebaseapp.com",
+    projectId: "product-hunt-d3f12",
+    storageBucket: "product-hunt-d3f12.appspot.com",
+    messagingSenderId: "667982336510",
+    appId: "1:667982336510:web:a3cba2d0d590edff9a70af",
+};
+
+export default firebaseConfig;
