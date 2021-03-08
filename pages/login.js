4378c2c5492b6532abde658e16c4be3b2b0ef237
DiegoMutre <tysonmutre10@gmail.com>
2021-03-08 05:42:01 -0500

    feat: Create validation for login

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,74 @@
+import styled from "styled-components";
 import Layout from "../components/layouts/layout";
+import { Error, Field, Form, InputSubmit } from "../components/ui/Form";
+import useValidation from "../hooks/useValidation";
+import validateCreateAccount from "../validation/validateCreateAccount";
+import firebase from "../firebase";
+import { useState } from "react";
+import router from "next/router";
+import validateLogin from "../validation/validateLogin";
+
+const LoginHeading = styled.h1`
+    text-align: center;
+    margin-top: 5rem;
+`;
+
+const initialState = {
+    email: "",
+    password: "",
+};
 
 const Login = () => {
+    const [error, setError] = useState("");
+
+    const {
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+        handleBlur,
+    } = useValidation(initialState, validateLogin, login);
+
+    const { email, password } = values;
+
+    function login() {
+        console.log("Login...");
+    }
+
     return (
         <>
             <Layout>
-                <h1>Login</h1>
+                <LoginHeading>Login</LoginHeading>
+                <Form onSubmit={handleSubmit} noValidate>
+                    <Field>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Your email"
+                            value={email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </Field>
+                    {errors.email && <Error>{errors.email}</Error>}
+                    <Field>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="Your password"
+                            value={password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </Field>
+                    {errors.password && <Error>{errors.password}</Error>}
+                    {error && <Error>{error}</Error>}
+                    <InputSubmit type="submit" value="Log in" />
+                </Form>
             </Layout>
         </>
     );
