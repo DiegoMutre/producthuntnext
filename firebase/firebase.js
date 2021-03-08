626c864c4d2715fbf8db82d424a2fe4e97a64e32
DiegoMutre <tysonmutre10@gmail.com>
2021-03-08 05:20:42 -0500

    feat: Create method register

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,11 +1,24 @@
 import app from "firebase/app";
 import firebaseConfig from "./config";
+import "firebase/auth";
 
 class Firebase {
     constructor() {
         if (!app.apps.length) {
             app.initializeApp(firebaseConfig);
         }
+        this.auth = app.auth();
+    }
+
+    async register(username, email, password) {
+        const newUser = await this.auth.createUserWithEmailAndPassword(
+            email,
+            password
+        );
+
+        return await newUser.user.updateProfile({
+            displayName: username,
+        });
     }
 }
 
