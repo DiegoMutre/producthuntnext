91689f3b7524efcde7f4d08adfa7c5577b491c88
DiegoMutre <tysonmutre10@gmail.com>
2021-03-08 05:03:39 -0500

    feat: Create the provider for firebase context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import firebase, { FirebaseContext } from "../firebase";
 import "../styles/app.css";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <FirebaseContext.Provider value={firebase}>
+            <Component {...pageProps} />
+        </FirebaseContext.Provider>
+    );
 }
 
 export default MyApp;
