44f5f796665067e42898ce6e664010cf7b73329c
DiegoMutre <tysonmutre10@gmail.com>
2021-03-09 07:45:34 -0500

    feat: Create 404.js

---
+++ components/layouts/404.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Heading = styled.h1`
+    margin-top: 5rem;
+    text-align: center;
+`;
+
+const Error404 = () => {
+    return <Heading>404 - Not found</Heading>;
+};
+
+export default Error404;
