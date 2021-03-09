5d7b1d32caecb6bb54ff54d1416f8ca246fb91a6
DiegoMutre <tysonmutre10@gmail.com>
2021-03-09 04:32:12 -0500

    feat: Get products from the DB

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,32 @@
+import { useContext, useEffect, useState } from "react";
 import Layout from "../components/layouts/layout";
+import firebase from "../firebase";
 
 export default function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = () => {
+            firebase.db
+                .collection("products")
+                .orderBy("created", "desc")
+                .onSnapshot(handleSnapshot);
+        };
+
+        function handleSnapshot(snapshot) {
+            const products = snapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data(),
+                };
+            });
+
+            setProducts(products);
+        }
+
+        getProducts();
+    }, []);
+
     return (
         <>
             <Layout>
