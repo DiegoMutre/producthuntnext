d47891276133e2efd47551f04348733d4af7b56b
DiegoMutre <tysonmutre10@gmail.com>
2021-03-09 07:21:29 -0500

    feat: Get id from url query

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,7 +1,21 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const Product = () => {
+    const router = useRouter();
+    const {
+        query: { id },
+    } = router;
+
+    useEffect(() => {
+        if (id) {
+            console.log("id ready");
+        }
+    }, [id]);
+
     return (
         <div>
-            <h1>From Product</h1>
+            <h1>From Product {id}</h1>
         </div>
     );
 };
