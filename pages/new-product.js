c844050f66e1777a0289663b425664e82174ec4d
DiegoMutre <tysonmutre10@gmail.com>
2021-03-10 05:28:36 -0500

    feat: Hide form for unauthenticated user

--- pages/new-product.js
+++ pages/new-product.js
@@ -7,6 +7,7 @@ import firebase, { FirebaseContext } from "../firebase";
 import { useContext, useState } from "react";
 import { useRouter } from "next/router";
 import FileUploader from "react-firebase-file-uploader";
+import Error404 from "../components/layouts/404";
 
 const NewProductHeading = styled.h1`
     text-align: center;
@@ -94,86 +95,98 @@ const NewProduct = () => {
     return (
         <>
             <Layout>
-                <NewProductHeading>New Product</NewProductHeading>
-                <Form onSubmit={handleSubmit} noValidate>
-                    <fieldset>
-                        <legend>General Information</legend>
-                        <Field>
-                            <label htmlFor="name">Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                id="name"
-                                placeholder="Your name"
-                                value={name}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            />
-                        </Field>
-                        {errors.name && <Error>{errors.name}</Error>}
+                {user ? (
+                    <>
+                        <NewProductHeading>New Product</NewProductHeading>
+                        <Form onSubmit={handleSubmit} noValidate>
+                            <fieldset>
+                                <legend>General Information</legend>
+                                <Field>
+                                    <label htmlFor="name">Name</label>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        id="name"
+                                        placeholder="Your name"
+                                        value={name}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                </Field>
+                                {errors.name && <Error>{errors.name}</Error>}
 
-                        <Field>
-                            <label htmlFor="company">Company</label>
-                            <input
-                                type="text"
-                                name="company"
-                                id="company"
-                                placeholder="Your company"
-                                value={company}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            />
-                        </Field>
-                        {errors.company && <Error>{errors.company}</Error>}
+                                <Field>
+                                    <label htmlFor="company">Company</label>
+                                    <input
+                                        type="text"
+                                        name="company"
+                                        id="company"
+                                        placeholder="Your company"
+                                        value={company}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                </Field>
+                                {errors.company && (
+                                    <Error>{errors.company}</Error>
+                                )}
 
-                        <Field>
-                            <label htmlFor="image">Image</label>
-                            <FileUploader
-                                name="image"
-                                id="image"
-                                accept="image/*"
-                                randomizeFilename
-                                storageRef={firebase.storage.ref("products")}
-                                onUploadStart={handleUploadStart}
-                                onUploadError={handleUploadError}
-                                onUploadSuccess={handleUploadSuccess}
-                                onProgress={handleUploadProgress}
-                            />
-                        </Field>
+                                <Field>
+                                    <label htmlFor="image">Image</label>
+                                    <FileUploader
+                                        name="image"
+                                        id="image"
+                                        accept="image/*"
+                                        randomizeFilename
+                                        storageRef={firebase.storage.ref(
+                                            "products"
+                                        )}
+                                        onUploadStart={handleUploadStart}
+                                        onUploadError={handleUploadError}
+                                        onUploadSuccess={handleUploadSuccess}
+                                        onProgress={handleUploadProgress}
+                                    />
+                                </Field>
 
-                        <Field>
-                            <label htmlFor="url">URL</label>
-                            <input
-                                type="url"
-                                name="url"
-                                id="url"
-                                placeholder="Your url"
-                                value={url}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            />
-                        </Field>
-                        {errors.url && <Error>{errors.url}</Error>}
-                    </fieldset>
-                    <fieldset>
-                        <legend>About your product</legend>
-                        <Field>
-                            <label htmlFor="description">Description</label>
-                            <textarea
-                                name="description"
-                                id="description"
-                                value={description}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            />
-                        </Field>
-                        {errors.description && (
-                            <Error>{errors.description}</Error>
-                        )}
-                    </fieldset>
-                    {error && <Error>{error}</Error>}
-                    <InputSubmit type="submit" value="Create Product" />
-                </Form>
+                                <Field>
+                                    <label htmlFor="url">URL</label>
+                                    <input
+                                        type="url"
+                                        name="url"
+                                        id="url"
+                                        placeholder="Your url"
+                                        value={url}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                </Field>
+                                {errors.url && <Error>{errors.url}</Error>}
+                            </fieldset>
+                            <fieldset>
+                                <legend>About your product</legend>
+                                <Field>
+                                    <label htmlFor="description">
+                                        Description
+                                    </label>
+                                    <textarea
+                                        name="description"
+                                        id="description"
+                                        value={description}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                </Field>
+                                {errors.description && (
+                                    <Error>{errors.description}</Error>
+                                )}
+                            </fieldset>
+                            {error && <Error>{error}</Error>}
+                            <InputSubmit type="submit" value="Create Product" />
+                        </Form>
+                    </>
+                ) : (
+                    <Error404 />
+                )}
             </Layout>
         </>
     );
