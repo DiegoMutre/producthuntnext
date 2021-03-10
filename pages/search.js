caeba3a4c41756cee7ff35762e42c956ce254506
DiegoMutre <tysonmutre10@gmail.com>
2021-03-10 18:51:57 -0500

    feat: Get query string

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,11 @@
 import Layout from "../components/layouts/Layout";
+import { useRouter } from "next/router";
 
 const Search = () => {
+    const {
+        query: { q },
+    } = useRouter();
+
     return (
         <>
             <Layout>
