2c2f8e1a4fb4fd7c4bf346a09bde0bcf3d7b7d5f
DiegoMutre <tysonmutre10@gmail.com>
2021-03-10 04:56:26 -0500

    styles: Modify Button.js

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 const Button = styled.a`
+    display: block;
     font-weight: 700;
+    text-align: center;
     text-transform: uppercase;
     border: 1px solid #d1d1d1;
     padding: 0.8rem 2rem;
-    margin-right: 1rem;
+    margin: 2rem auto;
     background-color: ${props => props.bgColor || "#fff"};
     color: ${props => (props.bgColor ? "#fff" : "#000")};
 
