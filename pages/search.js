1f21963315d114e2ce05e9db82b3d17a871905e7
DiegoMutre <tysonmutre10@gmail.com>
2021-03-10 19:05:45 -0500

    feat: Show search result

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,43 @@
 import Layout from "../components/layouts/Layout";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import useProducts from "../hooks/useProducts";
+import ProductDetails from "../components/layouts/ProductDetails";
 
 const Search = () => {
     const {
         query: { q },
     } = useRouter();
 
+    const { products } = useProducts("votes");
+
+    const [result, setResult] = useState([]);
+
+    useEffect(() => {
+        const search = q.toLowerCase();
+        const filter = products.filter(
+            product =>
+                product.name.toLowerCase().includes(search) ||
+                product.description.toLowerCase().includes(search)
+        );
+        setResult(filter);
+    }, [q, products]);
+
     return (
         <>
             <Layout>
-                <h1>Search</h1>
+                <div className="products-list">
+                    <div className="container">
+                        <ul className="bg-white">
+                            {result.map(product => (
+                                <ProductDetails
+                                    key={product.id}
+                                    product={product}
+                                />
+                            ))}
+                        </ul>
+                    </div>
+                </div>{" "}
             </Layout>
         </>
     );
